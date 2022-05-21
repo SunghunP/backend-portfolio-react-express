4482b4d393c4ec6e8ba5ce41f69f3d431b2034ef
Joe Park <sunghun.josephp@gmail.com>
2022-05-21 12:38:17 -0700

    Finish Backend

---
+++ server.js
@@ -0,0 +1,36 @@
+// Import dependencies
+const express = require("express");
+const cors = require("cors");
+
+// Import JSON files
+const projects = require("./projects.json");
+const about = require("./about.json");
+
+// Create app object
+const app = express();
+
+// MiddleWare
+app.use(cors());
+
+// Home Route for testing
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/projects/", (req, res) => {
+  // Send projects via JSON
+  res.json(projects);
+});
+
+app.get("/about", (req, res) => {
+  // Send about via JSON
+  res.json(about); 
+});
+
+// Declare PORT var
+const PORT = 3000;
+
+// Listen route
+app.listen(PORT, () => {
+  console.log(`Success! App is now running on port: ${PORT}`);
+});
